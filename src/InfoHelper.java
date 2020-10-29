9c79284f2d9c14eb4a667175f88d474981d726d8
alitarfa <alitarfa123@gmail.com>
2020-10-29 09:01:35 +0100

    refactor code

--- src/InfoHelper.java
+++ src/InfoHelper.java
@@ -5,42 +5,40 @@ import org.eclipse.jdt.core.dom.*;
 
 public class InfoHelper {
 
-    private static void className(ClassVisitors astVisitor) {
 
-        astVisitor.getClasses()
-                .forEach(typeDeclaration -> {
-                    System.out.println("Class " + typeDeclaration.getName());
-                    System.out.println("Parent Class" + typeDeclaration.getSuperclassType());
-                });
+    public static void show(ClassVisitors classVisitors) {
+        classVisitors.getClasses().forEach(InfoHelper::showClassInfo);
+        classVisitors.getClasses().forEach(InfoHelper::showMethodInfo);
+        classVisitors.getClasses().forEach(InfoHelper::showVariableInfo);
+    }
 
+    private static void showClassInfo(TypeDeclaration typeDeclaration) {
+        System.out.println("******* Class *******");
+        System.out.println("Class " + typeDeclaration.getName());
+        System.out.println("Parent Class" + typeDeclaration.getSuperclassType());
+    }
+
+    private static void showMethodInfo(TypeDeclaration typeDeclaration) {
         System.out.println("******* Methods *******");
-        astVisitor.getClasses()
-                .forEach(typeDeclaration -> {
-                    MethodVisitors methodVisitors = new MethodVisitors();
-                    typeDeclaration.accept(methodVisitors);
-                    methodVisitors
-                            .getMethods()
-                            .forEach(methodDeclaration -> {
-                                Type returnType = methodDeclaration.getReturnType2();
-                                SimpleName name = methodDeclaration.getName();
-                                System.out.println(returnType + " " + name);
-                            });
-                });
-
-        System.out.println("******* Variables *******");
-        astVisitor.getClasses()
-                .forEach(typeDeclaration -> {
-                    VariableVisitors variableVisitors = new VariableVisitors();
-                    typeDeclaration.accept(variableVisitors);
-                    variableVisitors
-                            .getFields()
-                            .stream()
-                            .map(VariableDeclaration::getName)
-                            .forEach(System.out::println);
+        MethodVisitors methodVisitors = new MethodVisitors();
+        typeDeclaration.accept(methodVisitors);
+        methodVisitors
+                .getMethods()
+                .forEach(methodDeclaration -> {
+                    Type returnType = methodDeclaration.getReturnType2();
+                    SimpleName name = methodDeclaration.getName();
+                    System.out.println(returnType + " " + name);
                 });
     }
 
-    public static void show(ClassVisitors classVisitors) {
-        className(classVisitors);
+    private static void showVariableInfo(TypeDeclaration typeDeclaration) {
+        System.out.println("******* Variables *******");
+        VariableVisitors variableVisitors = new VariableVisitors();
+        typeDeclaration.accept(variableVisitors);
+        variableVisitors
+                .getFields()
+                .stream()
+                .map(VariableDeclaration::getName)
+                .forEach(System.out::println);
     }
 }
